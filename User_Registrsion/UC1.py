fa16f94a0ed32f4148fa1472a0ce78a5d26e983a
GirishNekar <gmnekar18@gmail.com>
2024-08-14 16:43:19 +0530

    [Girish: Comitted Both UC1 and UC1_yest programs]

---
+++ User_Registrsion/UC1.py
@@ -0,0 +1,59 @@
+"""
+@Author: Girish
+@Date: 2024-08-14
+@Last Modified by: Girish
+@Last Modified time: 2024-08-14
+@Title: Check for Users First anme is Valid or not
+"""
+
+import re
+
+def validate_first_name(name: str) -> bool:
+    
+    
+    """
+    Descripton: 
+        Checks for the valid name
+    param name: 
+        The name to validate.
+    return: 
+        True if the name is valid, False otherwise.
+    """
+    
+    
+    pattern = r'^[A-Z][A-Za-z]{2,}$'
+    return bool(re.search(pattern, name))
+
+
+
+
+def get_user_input() -> str:
+    
+    
+    """
+    Description : 
+        Gets user input for the name.
+    Parameter : 
+        None
+    return: 
+        The name entered by the user.
+    """
+    
+    
+    return input("Enter the valid Name: ")
+
+
+
+def main():
+
+    name = get_user_input()
+    if validate_first_name(name):
+        print("Valid Name")
+    else:
+        print("Invalid Name")
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ User_Registrsion/UC1_test.py
@@ -0,0 +1,45 @@
+"""
+@Author: Girish
+@Date: 2024-08-14
+@Last Modified by: Girish
+@Last Modified time: 2024-08-14
+@Title: Check for Users First anme is Valid or not
+"""
+
+import unittest
+from UC1 import validate_first_name  
+
+class TestNameValidation(unittest.TestCase):
+    """
+    Unit test class for validating name functionality.
+    
+    """
+
+    def test_valid_name(self):
+        
+        
+        """
+        Description:
+            Checks if the name is valid according to the validation criteria.
+        Parameters:
+            self
+        Return:
+            Returns True for a valid name.
+        """
+        
+        
+        self.assertTrue(validate_first_name("John"),"Should be valid")
+        self.assertTrue(validate_first_name("Alice"),"Should be valid")
+        self.assertTrue(validate_first_name("Jo"), "Should be invalid due to length")
+        self.assertFalse(validate_first_name("Al"), "Should be invalid due to length")
+        self.assertFalse(validate_first_name("john"),"Should be invalid due to lowercase first letter")
+        self.assertFalse(validate_first_name("alice"),"Should be invalid due to lowercase first letter")
+        self.assertFalse(validate_first_name("Jo@n"), "Should be invalid due to  presence of a special character")
+        self.assertFalse(validate_first_name("Al!ce"),  "Should be invalid due to  presence of a special character")
+        self.assertFalse(validate_first_name("John123"), "Should be invalid due to  presence of a Number")
+        self.assertFalse(validate_first_name("Alice456"), "Should be invalid due to  presence of a Number")
+
+
+
+if __name__ == "__main__":
+    unittest.main()
